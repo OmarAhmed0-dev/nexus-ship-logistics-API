35f28ee8ac4403742ba696681c4d8ecff265c82d
Omar Ahmed <omarahmed0.dev@gmail.com>
2026-05-13 11:18:35 +0300

    feat(exception): Add a catch-all global exception handler for internal server errors.

--- src/main/java/com/nexus/NexusShip/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nexus/NexusShip/exception/GlobalExceptionHandler.java
@@ -3,11 +3,13 @@ package com.nexus.NexusShip.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import com.nexus.NexusShip.dto.response.ErrorResponse;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -31,5 +33,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleInvalidPhoneNumber(InvalidPhoneNumber ex) {
         return buildErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
 
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        return buildErrorResponse(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    //Handle not expected exception
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
+        return buildErrorResponse("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
